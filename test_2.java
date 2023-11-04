275eaf6e4115218df2f8ecc1fc668b7c911dafc1
Muvinn <kbvi45@gmail.com>
2023-11-05 03:15:14 +0900

    commit

--- test_2.java
+++ test_2.java
@@ -2,5 +2,6 @@ public class test_2 {
     public static void main(String[] args) {
         System.out.println("안녕하세용");
         System.out.println("이제 작동하려나?");
+        System.out.println("앗싸 저장 완료된다리~");
     }
 }
